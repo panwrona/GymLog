2806b93ca0c38d88e084009ad6b2e3d252fb1b19
mariusz.brona <mariusz.brona@droidsonroids.pl>
2017-09-05 23:55:46 +0200

    Move MainView to seperate class

--- app/src/main/java/com/panwrona/gymlog/ui/screen/main/MainActivity.kt
+++ app/src/main/java/com/panwrona/gymlog/ui/screen/main/MainActivity.kt
@@ -3,11 +3,9 @@ package com.panwrona.gymlog.ui.screen.main
 import com.panwrona.gymlog.GymLogApp
 import com.panwrona.gymlog.R
 import com.panwrona.gymlog.common.mvp.MvpActivity
-import com.panwrona.gymlog.common.mvp.MvpView
 
 class MainActivity: MvpActivity<MainView, MainPresenter>(), MainView {
     override val layout = R.layout.activity_main
     override fun getDependencies() = GymLogApp.appComponent.inject(this)
 }
 
-interface MainView: MvpView
