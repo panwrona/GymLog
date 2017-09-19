aab7f78bd368317c9d77248f9ba2a7f577207f22
mariusz.brona <mariusz.brona@droidsonroids.pl>
2017-09-20 00:07:40 +0200

    Fix android versions and sdk manager path in emulator script

--- ci/emulator.sh
+++ ci/emulator.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sdkmanager tools
-sdkmanager "system-images;android-25;default;armeabi-v7a"
+$ANDROID_HOME/tools/bin/sdkmanager tools
+$ANDROID_HOME/tools/bin/sdkmanager "system-images;android-25;default;armeabi-v7a"
 echo no | avdmanager create avd --force -n test -k "system-images;android-25;default;armeabi-v7a"
 $ANDROID_HOME/emulator/emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
