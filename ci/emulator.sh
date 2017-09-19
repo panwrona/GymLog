f9e9f161feac817157167cf9e18597ff950e90f3
mariusz.brona <mariusz.brona@droidsonroids.pl>
2017-09-19 23:58:22 +0200

    Add another configuration because of issue shown here: https://github.com/JakeWharton/butterknife/pull/1055

--- ci/emulator.sh
+++ ci/emulator.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a --skin 480x800
-emulator -avd test -no-audio -no-window &
+sdkmanager tools
+sdkmanager "system-images;android-25;default;armeabi-v7a"
+echo no | avdmanager create avd --force -n test -k "system-images;android-25;default;armeabi-v7a"
+$ANDROID_HOME/emulator/emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82
