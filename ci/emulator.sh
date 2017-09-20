806aae8ed322ba11766d7bd4969f45f8e2a2f2c5
mariusz.brona <mariusz.brona@droidsonroids.pl>
2017-09-20 08:17:20 +0200

    Change before install script

--- ci/emulator.sh
+++ ci/emulator.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir "$ANDROID_HOME/licenses" || true
-echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
-sdkmanager tools
-sdkmanager "system-images;android-25;default;armeabi-v7a"
+$ANDROID_HOME/tools/bin/sdkmanager tools
+$ANDROID_HOME/tools/bin/sdkmanager "system-images;android-25;default;armeabi-v7a"
 echo no | avdmanager create avd --force -n test -k "system-images;android-25;default;armeabi-v7a"
 $ANDROID_HOME/emulator/emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
