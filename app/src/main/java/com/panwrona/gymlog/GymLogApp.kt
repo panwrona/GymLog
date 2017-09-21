1770926e7f203aa8d405ec95a1be055b8123a2cd
mariusz.brona <mariusz.brona@droidsonroids.pl>
2017-09-21 11:52:02 +0200

    Remove calligraphy library

--- app/src/main/java/com/panwrona/gymlog/GymLogApp.kt
+++ app/src/main/java/com/panwrona/gymlog/GymLogApp.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import com.panwrona.gymlog.common.dagger.component.AppComponent
 import com.panwrona.gymlog.common.dagger.component.DaggerAppComponent
 import com.panwrona.gymlog.common.dagger.module.AppModule
-import uk.co.chrisjenx.calligraphy.CalligraphyConfig
 
 class GymLogApp : Application() {
     companion object {
@@ -15,15 +14,7 @@ class GymLogApp : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
-        initCalligraphy()
         initAppComponent()
-    }
-
-    private fun initCalligraphy() {
-        CalligraphyConfig.initDefault(CalligraphyConfig.Builder()
-                .setDefaultFontPath("font/montserrat_regular.ttf")
-                .setFontAttrId(R.attr.fontPath)
-                .build())
     }
 
     private fun initAppComponent() {
