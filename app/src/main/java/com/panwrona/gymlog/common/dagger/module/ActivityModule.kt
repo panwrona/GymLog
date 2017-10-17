d9ad972c65ea714d6f0f7c2956810e4dbd2d0944
mariusz.brona <mariusz.brona@droidsonroids.pl>
2017-10-17 19:09:41 +0200

    Updated gradle files, build tools and kotlin plugin

--- app/src/main/java/com/panwrona/gymlog/common/dagger/module/ActivityModule.kt
+++ app/src/main/java/com/panwrona/gymlog/common/dagger/module/ActivityModule.kt
@@ -7,16 +7,4 @@ import dagger.Provides
 @Module
 class ActivityModule(val activity: Activity) {
 
-    //TODO uncomment when facebook added
-//    @Provides
-//    @PerActivity
-//    fun provideCallbackManager(): CallbackManager {
-//        return CallbackManager.Factory.create()
-//    }
-//
-//    @Provides
-//    @PerActivity
-//    fun provideFacebookStore(callbackManager: CallbackManager): FacebookStore {
-//        return FacebookStoreImpl(activity, callbackManager)
-//    }
 }
