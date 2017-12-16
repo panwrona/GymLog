fca7691110d8b9355650afc1c68a6537a009be3b
mariusz.brona <mariusz.brona@droidsonroids.pl>
2017-12-16 13:07:10 +0100

    [D][FT][8][9][Configure build.gradle for Bitrise CI and configure Bitrise CI Workflow]

--- ci/emulator.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-$ANDROID_HOME/tools/bin/sdkmanager tools
-$ANDROID_HOME/tools/bin/sdkmanager "system-images;android-25;default;armeabi-v7a"
-echo no | avdmanager create avd --force -n test -k "system-images;android-25;default;armeabi-v7a"
-$ANDROID_HOME/emulator/emulator -avd test -no-audio -no-window &
-android-wait-for-emulator
-adb shell input keyevent 82
